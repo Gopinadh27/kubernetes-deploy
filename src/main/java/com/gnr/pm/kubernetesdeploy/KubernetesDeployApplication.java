50d6ee04cb69a126aa135c77f0d1e2bcd02beb75
Gopinadh Reddy <chinthareddy.gopinadh@ovaledge.com>
2025-04-09 01:00:30 +0530

    first commit

---
+++ src/main/java/com/gnr/pm/kubernetesdeploy/KubernetesDeployApplication.java
@@ -0,0 +1,13 @@
+package com.gnr.pm.kubernetesdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KubernetesDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KubernetesDeployApplication.class, args);
+    }
+
+}
